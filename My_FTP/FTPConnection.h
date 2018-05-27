98dff62838b445991bdd9c7cd59c5959cd63437f
kimninh <1612484@student.hcmus.edu.vn>
2018-05-27 23:17:23 +0700

    - Hàm cuối cùng đã xong (mput) (vẫn còn đang test)

--- My_FTP/FTPConnection.h
+++ My_FTP/FTPConnection.h
@@ -62,6 +62,7 @@ public:
 	BOOL DeleteRemoteMultipleFiles(const vector<CString>& remote_file_names);
 	BOOL RemoveRemoteDir(const CString& remote_dir);
 	BOOL PrintRemoteWorkingDir();
+	BOOL PutMultipleFiles(const vector<CString> &localFile);
 };
 
 
