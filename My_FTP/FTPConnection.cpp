6e7c0c1e887d286b96b1d99172be30f6b53e5aaf
kimninh <1612484@student.hcmus.edu.vn>
2018-05-31 11:09:26 +0700

    Các thay đổi: + splitLineToQueue() -> dùng để tách msg nhận đc khi dính liền 150 và 226 + Tự ngắt kết nối khi quá thời gian chờ đăng nhập + remote address sẽ được tự động check, bỏ đi hàm thủ công + Tối ưu lại đoạn code gửi data file (hàm put_file()) +Đổi tên  LoadArgIntoQueue

--- My_FTP/FTPConnection.cpp
+++ My_FTP/FTPConnection.cpp
@@ -22,7 +22,19 @@ void splitLineToVector(const char *src, vector<CString> &des)
 	}
 }
 
+void splitLineToQueue(queue<CString> &des, CString src)
+{
+	static CString buff;
+	buff += src;
+	int length = buff.Find("\r\n");
 
+	while (length != -1)
+	{
+		des.push(buff.Left(length + 2));
+		buff.Delete(0, length + 2);
+		length = buff.Find("\r\n");
+	}
+}
 
 BOOL FTPConnection::InitDataSock()
 {
@@ -97,11 +109,9 @@ CString FTPConnection::get_path(const CString & fullPath)
 {
 	char drive[_MAX_DRIVE];
 	char dir[_MAX_DIR];
-	char fname[_MAX_FNAME];
-	char ext[_MAX_EXT];
 
 	errno_t err;
-	err = _splitpath_s(fullPath.GetString(), drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, ext, _MAX_EXT);
+	err = _splitpath_s(fullPath.GetString(), drive, _MAX_DRIVE, dir, _MAX_DIR, NULL, 0, NULL, 0);
 
 	// Thiếu cả tên ổ cứng và thư mục chứa -> dùng đường dẫn hiện tại
 	if (CString(drive).IsEmpty() && CString(dir).IsEmpty())
@@ -109,7 +119,7 @@ CString FTPConnection::get_path(const CString & fullPath)
 
 	//Chỉ thiếu tên ổ cứng -> thư mục con của đường dẫn hiện tại
 	if (CString(drive).IsEmpty())
-		return currentDir + (CString)dir;
+		return currentDir + "\\" + (CString)dir;
 
 	//Đủ cả tên ổ cứng và thư mục
 	return (CString)drive + (CString)dir;
@@ -117,13 +127,11 @@ CString FTPConnection::get_path(const CString & fullPath)
 
 CString FTPConnection::get_fileName_with_Ext(const CString & fullPath)
 {
-	char drive[_MAX_DRIVE];
-	char dir[_MAX_DIR];
 	char fname[_MAX_FNAME];
 	char ext[_MAX_EXT];
 
 	errno_t err;
-	err = _splitpath_s(fullPath.GetString(), drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, ext, _MAX_EXT);
+	err = _splitpath_s(fullPath.GetString(), NULL, 0, NULL, 0, fname, _MAX_FNAME, ext, _MAX_EXT);
 
 	return (CString)fname + (CString)ext;
 }
@@ -167,25 +175,23 @@ BOOL FTPConnection::OpenConnection(const char * IPAddr)
 		outputControlMsg.push(CString(msg));
 	}
 
-	auto isValidIPAddr = [](const char *IPAddr) -> bool
-	{
-		UINT a, b, c, d;
-		if (sscanf_s(IPAddr, "%u.%u.%u.%u", &a, &b, &c, &d) != 4)
-			return false;
-		return a < 256 && b < 256 && c < 256 && d < 256;
-	};
-
-	if (!isValidIPAddr(IPAddr)) {
-		outputControlMsg.push(CString("Unknow host\n"));
-		return FALSE;
-	}
-
 	if (!controlSock.Connect(IPAddr, 21)) {
-		sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
+		switch (controlSock.GetLastError())
+		{
+		case WSAETIMEDOUT:
+			strcpy_s(msg, "Connection time out.\r\n");				// Tự thoát khi quá thời gian chờ
+			break;
+		case WSAEINVAL:
+			strcpy_s(msg, "Unknown host.\r\n");						// Tự kiểm tra địa chỉ IP hợp lệ
+			break;
+		}
+
 		outputControlMsg.push(CString(msg));
+		controlSock.Close();
 		return FALSE;
 	}
 
+	// Thông báo đã kết nối thành công tới IPAddr
 	sprintf_s(msg, "Connected to %s.\n", IPAddr);
 	outputControlMsg.push(CString(msg));
 
@@ -476,12 +482,9 @@ BOOL FTPConnection::ListAllDirectory(const char * remote_dir, const char * local
 	}
 
 	msg[msgSz] = '\0';
-	outputControlMsg.push(msg);
-
-	bool isReceivedEnough = false;
-	if ((CString(msg)).Find("\r\n") != msgSz - 2)
-		isReceivedEnough = true;
-
+	// Trường hợp mà nhận đc cả hai thông điệp 150 ... \r\n và 126... \r\n
+	// ->Tách ra rồi đưa vào hàng đợi
+	splitLineToQueue(outputControlMsg, msg);
 
 	int relyCode = getRelyCode(msg);
 	if (relyCode != 150 && relyCode != 125)
@@ -490,29 +493,28 @@ BOOL FTPConnection::ListAllDirectory(const char * remote_dir, const char * local
 	// Nhận data ở cả active và passive
 	char data[MAX_TRANSFER] = { 0 };
 	int dataSz;
-	CString buff;
 
+	//Gửi lần lượt từng block data lên server
 	while ((dataSz = dataTrans.Receive(data, MAX_TRANSFER - 1)) > 0)
 	{
 		data[dataSz] = '\0';
 		splitLineToVector(data, outputMsg);
 	}
+	close_data_sock();
 
-	dataTrans.Close();
-	if (!isPassive)
-		dataSock.Close();
+	// Lấy thử thông điệp cuối cùng -> Đã nhận "226 ...\r\n" thì thoát
+	if (getRelyCode(outputControlMsg.back().GetString()) == 226)
+		return true;
 
-	if (!isReceivedEnough)
-	{
-		if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
-			sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
-			outputControlMsg.push(CString(msg));
-			return false;
-		}
-		msg[msgSz] = '\0';
-		outputControlMsg.push(msg);
+	// Nếu vẫn chưa nhận thông điệp "226 ...\r\n"
+	// -> Nhận cho đủ
+	if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
+		sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
+		outputControlMsg.push(CString(msg));
+		return false;
 	}
-
+	msg[msgSz] = '\0';
+	outputControlMsg.push(msg);
 	return true;
 }
 
@@ -624,90 +626,69 @@ BOOL FTPConnection::PutFile(const CString& localFile, const CString& remoteFile)
 			file.close();
 			return FALSE;
 		}
-		else
+		
+		if (!dataSock.Accept(dataTrans))
 		{
-			if (!dataSock.Accept(dataTrans))
-			{
-				sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
-				outputControlMsg.push(CString(msg));
-				file.close();
-				return FALSE;
-			}
+			sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
+			outputControlMsg.push(CString(msg));
+			file.close();
+			return FALSE;
 		}
 	}
-
-
-	if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
-		sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
-		outputControlMsg.push(CString(msg));
-		return false;
-	}
-
-	msg[msgSz] = '\0';
-	outputControlMsg.push(msg);
-
-	bool isReceivedEnough = false;
-	if ((CString(msg)).Find("\r\n") != msgSz - 2)
-		isReceivedEnough = true;
-
-	if (isPassive)
+	else
 	{
 		if (!dataTrans.Connect(serverIP.GetString(), serverPort))
 		{
 			sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
 			outputControlMsg.push(CString(msg));
 			file.close();
+			close_data_sock();
 			return false;
 		}
 	}
+
+	if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
+		sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
+		outputControlMsg.push(CString(msg));
+		close_data_sock();
+		return false;
+	}
+
+	msg[msgSz] = '\0';
+	splitLineToQueue(outputControlMsg, msg);
 
 	int relyCode = getRelyCode(msg);
 	if (relyCode != 150 && relyCode != 125)
 	{
 		file.close();
+		close_data_sock();
 		return false;
 	}
 	// Gửi data ở cả active và passive
 	char data[MAX_TRANSFER - 1] = { 0 };
 	int dataSz;
-	int fileSz;
-	int nLoop;
-
-	file.seekg(0, file.end);
-	fileSz = file.tellg();
-	file.seekg(0, file.beg);
-
-	nLoop = fileSz / (MAX_TRANSFER - 1);
-	if (fileSz % (MAX_TRANSFER - 1) != 0)
-		nLoop += 1;
-
-	for (int i = 0; i < nLoop; i++)
+	while (!file.eof())
 	{
-		if (i == nLoop - 1 && fileSz % (MAX_TRANSFER - 1) != 0)
-			dataSz = fileSz % (MAX_TRANSFER - 1);
-		else
-			dataSz = MAX_TRANSFER - 1;
 		file.read(data, MAX_TRANSFER - 1);
+		dataSz = file.gcount();
 		dataTrans.Send(&data, dataSz);
 	}
 	file.close();
+	close_data_sock();
+	
+	// Lấy thử thông điệp cuối cùng -> Đã nhận "226 ...\r\n" thì thoát
+	if (getRelyCode(outputControlMsg.back().GetString()) == 226)
+		return true;
 
-	dataTrans.Close();
-	if (!isPassive)
-		dataSock.Close();
-
-	if (!isReceivedEnough)
-	{
-		if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
-			sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
-			outputControlMsg.push(CString(msg));
-			file.close();
-			return false;
-		}
-		msg[msgSz] = '\0';
-		outputControlMsg.push(msg);
+	// Nếu vẫn chưa nhận thông điệp "226 ...\r\n"
+	// -> Nhận cho đủ
+	if ((msgSz = controlSock.Receive(msg, MAX_MSG_BUF)) <= 0) {
+		sprintf_s(msg, "Error code: %d\n", controlSock.GetLastError());
+		outputControlMsg.push(CString(msg));
+		return false;
 	}
-
+	msg[msgSz] = '\0';
+	outputControlMsg.push(msg);
 	return true;
 }
 
@@ -1090,7 +1071,7 @@ BOOL FTPConnection::PutMultipleFiles(const vector<CString>& localFile)
 	BOOL bRet = TRUE;
 
 	char sPath[256];
-
+	char msg[MAX_MSG_BUF];
 	for (auto it : localFile)
 	{
 
@@ -1100,14 +1081,20 @@ BOOL FTPConnection::PutMultipleFiles(const vector<CString>& localFile)
 		HANDLE hFind = nullptr;
 
 		//Tìm thấy tên file thỏa trong thư mục
-		if ((hFind = FindFirstFile(sPath, &fdFile)) != INVALID_HANDLE_VALUE)
+		if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
 		{
-			do
-			{
-				bRet = PutFile(get_path(it) + fdFile.cFileName, "");
-			} while (FindNextFile(hFind, &fdFile));
-			FindClose(hFind);
+			sprintf_s(msg, "%s : File not found\r\n", it.GetString());
+			outputControlMsg.push(CString(msg));
+			bRet = FALSE;
+			continue;
 		}
+
+		do
+		{
+			// Nếu tồn tại 1 file ko upload được -> ghi nhận vào biến bRet, tiếp tục thử các file tiếp theo
+			bRet = PutFile(get_path(it) + fdFile.cFileName, "") && bRet;
+		} while (FindNextFile(hFind, &fdFile));
+		FindClose(hFind);
 	}
 
 	return bRet;
